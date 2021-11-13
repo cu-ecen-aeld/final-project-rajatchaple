4ec7c2e4a4ac699f3576d3b556d7b136d41eaed7
sundar <sundar.krishnakumar@colorado.edu>
2021-11-12 22:48:27 -0700

    re-arranged directories in code directory

---
+++ code/simple-driver/adc_char_driver.h
@@ -0,0 +1,32 @@
+#ifndef ADC_SPI_CHAR_H
+#define ADC_SPI_CHAR_H
+
+#include <linux/spi/spi.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/kernel.h> // for sprintf()
+#include <linux/string.h>
+
+#include "low_level_driver.h"
+
+#define ADC_DEBUG 1 //Remove comment on this line to enable debug
+
+#undef PDEBUG /* undef it, just in case */
+#ifdef ADC_DEBUG
+#ifdef __KERNEL__
+/* This one if debugging is on, and kernel space */
+#define PDEBUG(fmt, args...) printk(KERN_DEBUG "adc_spi: " fmt, ##args)
+#else
+/* This one for user space */
+#define PDEBUG(fmt, args...) fprintf(stderr, fmt, ##args)
+#endif
+#else
+#define PDEBUG(fmt, args...) /* not debugging: nothing */
+#endif
+
+int chrdev_init(struct omap2_mcspi *lmcspi);
+void chrdev_exit(void);
+
+#endif  //ADC_SPI_CHAR_H
---
+++ code/simple-driver/adc_spi.mod.c
@@ -0,0 +1,53 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x972f93ab, "module_layout" },
+	{ 0x215f7233, "cdev_del" },
+	{ 0xe3b72f06, "cdev_init" },
+	{ 0xc8ab5383, "device_destroy" },
+	{ 0x6091b333, "unregister_chrdev_region" },
+	{ 0x3c3ff9fd, "sprintf" },
+	{ 0x15ba50a6, "jiffies" },
+	{ 0x6b10bee1, "_copy_to_user" },
+	{ 0xc5850110, "printk" },
+	{ 0xde80cd09, "ioremap" },
+	{ 0x712605e5, "device_create" },
+	{ 0xa89d7d2, "cdev_add" },
+	{ 0xa916b694, "strnlen" },
+	{ 0xc959d152, "__stack_chk_fail" },
+	{ 0xbdfb6dbb, "__fentry__" },
+	{ 0xcbd4898c, "fortify_panic" },
+	{ 0xad39bb2b, "class_destroy" },
+	{ 0x5b54da1d, "__class_create" },
+	{ 0x88db9f48, "__check_object_size" },
+	{ 0xe3ec2f2b, "alloc_chrdev_region" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "0262F6E3233A6405D6E426B");
